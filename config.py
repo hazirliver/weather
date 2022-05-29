5c3b62241656ad612359c706e36ce9a875d90430
hazirliver <cs-mitino@mail.ru>
2022-05-29 20:34:51 +0400

    Added README

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 USE_ROUNDED_COORDS = True
-OPENWEATHER_API = "efe096c5d1e8a2bb6b45f277194631ee"
+OPENWEATHER_API = "" # paste API token here
 OPENWEATHER_URL = ("https://api.openweathermap.org/data/2.5/weather?"
                    "lat={latitude}&lon={longitude}&"
                    "appid=" + OPENWEATHER_API + "&lang=ru&"
